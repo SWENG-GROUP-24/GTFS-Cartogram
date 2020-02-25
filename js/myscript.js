b4bf283ca722e82d1bbb28d63153a78feb16ea2e
Tom Moran <32645063+MySupersuit@users.noreply.github.com>
2020-02-25 17:26:36 +0000

    Changed Markers to MarkerCircles
    MarkerCircles look cleaner and are native to leaflet

--- js/myscript.js
+++ js/myscript.js
@@ -1,70 +1,75 @@
 markers = [];
-
-var mymap = L.map('mapid').setView([53.5, -7.9],7); // Leaflet Map Object centred on Ireland
-
 mapbox_apikey = 'pk.eyJ1IjoiYm9idG9tODQ4MiIsImEiOiJjazZzMXh1YmEwYmJrM2ZtbHU1dm5pZW92In0.5070Es5hbrpyO-li0XagsA'; 
 thunderforest_apikey = '030ae5a7c98549079b98e1a051b27cb7';
+var mymap;
+var myRenderer;
+var centre = {
+	ireland: [53.5,-7.9],
+	switzerland: [47, 8.3]
+};
+var zoom = {
+	ireland: 7,
+	switzerland: 7
+}
 
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYm9idG9tODQ4MiIsImEiOiJjazZzMXh1YmEwYmJrM2ZtbHU1dm5pZW92In0.5070Es5hbrpyO-li0XagsA', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoiYm9idG9tODQ4MiIsImEiOiJjazZzMXh1YmEwYmJrM2ZtbHU1dm5pZW92In0.5070Es5hbrpyO-li0XagsA'
-// }).addTo(mymap);
+function start() {
+	makeMap();
+}
 
-// Creating base layer of map (more designs on line)
-var Thunderforest_TransportDark = L.tileLayer(
-	'https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey='+thunderforest_apikey, {
-	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	apikey: thunderforest_apikey,
-	maxZoom: 22
-}).addTo(mymap);
+function makeMap() {
+	mymap = L.map('mapid').setView(centre.ireland,zoom.ireland);	
 
+	var Thunderforest_TransportDark = L.tileLayer(
+		'https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey='+thunderforest_apikey, {
+		attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		apikey: thunderforest_apikey,
+		maxZoom: 22
+	}).addTo(mymap);
 
-$.ajax({
-	url : "https://raw.githubusercontent.com/MySupersuit/leaflet_test/master/js/assets/trains/stops.txt",
-	success : function (data) {
-		parseCSV(data);
-	}
-});
+	myRenderer = L.canvas({padding: 0.5});
+
+	getData();
+
+}
+
+function getData() {
+	$.ajax({
+		url : "https://raw.githubusercontent.com/MySupersuit/leaflet_test/master/js/assets/trains/stops.txt",
+		// url : "https://raw.githubusercontent.com/SWENG-GROUP-24/GTFS-Cartogram/lineSegments/js/assets/trains/swiss_stops.txt",
+		success : function (data) {
+			parseCSV(data);
+		}
+	});
+}
 
 function parseCSV(csv) {
 	parsed = Papa.parse(csv);
-	createMarkers(parsed);
+	plotPoints(parsed);
 }
 
-
-function createMarkers(csv) {
-	// for (let i = 1; i < csv.data.length-1; i++) {
-		lat = csv.data[1][2];
-		long = csv.data[1][3];
-		console.log(lat + " " + long);
-		let coord = L.latLng(lat,long)
-		let marker = new L.marker(coord);
-		markers.push(marker);
-		marker.addTo(mymap);
-
-		connollyX = 53.3524594780812;
-		connollyY  = -6.24708495616048;
-
-		m2 = new L.marker(L.latLng(connollyX,connollyY));
-		m2.addTo(mymap);
-
-		newP = lineDividing(lat, long, connollyX, connollyY, 1,-5);
-
-		console.log(newP);
-
-		m1 = new L.marker(L.latLng(newP.p1, newP.p2));
-
-		m1.addTo(mymap);
-	// }
+function plotPoints(csv) {
+	for (let i = 1; i < csv.data.length-1; i++) {
+		lat = csv.data[i][2];
+		long = csv.data[i][3];
+		let coord = L.latLng(lat,long);
+		L.circleMarker(coord, {
+			renderer: myRenderer,
+			radius: 1
+		}).addTo(mymap).bindPopup(csv.data[i][1]);
+	}
 }
 
+/*
+	Given two points and a ratio a:b finds the point
+	which splits the line in ratio a:b
+
+	Make b constant for one less variable?
+	Start station will always be the same 
+*/
 function lineDividing(x1, y1, x2, y2, a, b) {
 	p1 = (b*x1 + a*x2)/(a+b);
 	p2 = (b*y1 + a*y2)/(a+b);
 	return {p1,p2};
 }
 
+start();
