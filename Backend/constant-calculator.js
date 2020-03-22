deeee9672b978b1f8a210d88c9ae4f0632da1f3e
MendesRafa <rafaman089@outlook.com>
2020-03-22 11:48:42 +0000

    redesigned the algorithm for calculating the average distance between two stops

--- Backend/constant-calculator.js
+++ Backend/constant-calculator.js
@@ -3,58 +3,75 @@ var parse = require('csv-parse');
 
 var inputFile='gtfs_data/shapes.txt';
 
-var lastDistance = 0;
+var shape = [];
+var currentShapeID = '';
+var lastShapeID = '';
+
 var currentDistance = 0;
+
 var accDistance = 0;
+
 var numberOfDistances = 0;
+
 var averageDistance = 0;
-var maxDistance = 0;
-var minDistance = Number.MAX_SAFE_INTEGER;
-var range = 0;
 
 var parser = parse({delimiter: ','}, function (err, data){
   data.forEach(function(line){
     //makes sure that headers are not selected
-    if (line[0]==="shape_id"){
+    if (line[1]==="shape_pt_lat"){
       return true;
     }
 
-    currentDistance = parseFloat(line[4]);
+    lat = parseFloat(line[1]);
+    lon = parseFloat(line[2]);
 
-    //makes sure the last line isn't used 
-    if (currentDistance !== currentDistance) {
-      return true;
-    }
+    currentShapeID = line[0];
 
-    //if currentDistance is set back to 0 that means
-    //we are getting distances from a new route 
-    //so we reset
-    if (currentDistance == 0) {
-      lastDistance = currentDistance;
-      return true;
+    if (currentShapeID!==lastShapeID && shape !== undefined && shape.length != 0) {
+      for (var i=0; i<shape.length; i++) {
+        for (var j=i+1; j<shape.length; j++) {
+          currentDistance=calculateDistance(shape[i].lat, shape[i].lon, shape[j].lat, shape[j].lon);
+          if (currentDistance!=0){
+            accDistance=accDistance+currentDistance;
+            numberOfDistances++;
+          }
+        }
+      }
+      shape = [];
     }
-    //otherwise to get a distance between two stops 
-    //we must take away the last distance from the current one
-    //as 
-    else {
-      accDistance = accDistance + (currentDistance - lastDistance);
-      lastDistance = currentDistance;
-      numberOfDistances++;
-    }
-
-    if (currentDistance > maxDistance) {
-      maxDistance=currentDistance;
-    }
-
-    if ((currentDistance - lastDistance) < minDistance) {
-      minDistance= (currentDistance - lastDistance);
-    }
-    
+    lastShapeID = currentShapeID;
+    shape.push({"lat":lat, "lon":lon});
   });
-  averageDistance = accDistance/numberOfDistances;
-  range = maxDistance - minDistance;
-  console.log(range);
+  for (var i=0; i<shape.length; i++) {
+    for (var j=i+1; j<shape.length; j++) {
+      currentDistance=calculateDistance(shape[i].lat, shape[i].lon, shape[j].lat, shape[j].lon);
+      if (currentDistance!=0){
+        accDistance=accDistance+currentDistance;
+        numberOfDistances++;
+      }
+    }
+  }
+
+  averageDistance=accDistance/numberOfDistances;
   console.log(averageDistance);
 });
 
 fs.createReadStream(inputFile).pipe(parser);
+
+function calculateDistance (lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
